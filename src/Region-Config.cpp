01279368f0ac2b9361f20d705df78c697e2c6cd4
Alec Tackitt <100729347+ATack-45@users.noreply.github.com>
2024-12-17 15:42:03 -0800

    Update Region-Config.cpp

--- src/Region-Config.cpp
+++ src/Region-Config.cpp
@@ -1,30 +1,40 @@
 #include "Region-config.h"
 #include "lemlib/api.hpp"
 #include "main.h" 
+#include "pros/adi.hpp"
+#include "pros/motor_group.hpp"
+#include "pros/motors.hpp"
+#include "pros/optical.hpp"
 
-// create motor for intake on port 2
-pros::Motor intake(14);
+// creating devices
+pros::Motor intake(-11);
+pros::MotorGroup lift_motors({-5, 7});
+pros::adi::DigitalOut Claw(8);
+pros::adi::DigitalOut Blooper(1);
 
-pros::ADIDigitalOut Claw(8);
-pros::ADIPotentiometer drive_select(2);
-pros::ADIPotentiometer auto_select (3);
+
+//creating sensors
+pros::adi::Potentiometer auto_select (7);
+pros::Optical optical_sensor(19);
+pros::Rotation wall_arm(16);
+
 
 
 // drivetrain groups
-pros::MotorGroup left_motors({-20, -19, 18}, pros::MotorGearset::blue); // left motors on ports 1, 2, 3
-pros::MotorGroup right_motors({10, 9, -8}, pros::MotorGearset::blue); // right motors on ports 4, 5, 6
+pros::MotorGroup left_motors({10, -9, -8}, pros::MotorGearset::blue); // left motors on ports 1, 2, 3
+pros::MotorGroup right_motors({-1, 2, 3}, pros::MotorGearset::blue); // right motors on ports 4, 5, 6
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motors, // left motor group
                               &right_motors, // right motor group
-                              10, // 10 inch track width
-                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
+                              10.5, // 10 inch track width
+                              lemlib::Omniwheel::NEW_325, // using new 3.25" omnis
                               360, // drivetrain rpm is 360
                               2 // horizontal drift is 2 (for now)
 );
 
-// create an imu on port 10
-pros::Imu imu(15);
+// create an imu on port 20
+pros::Imu imu(20);
 //assigning drivetrain as vertical tracking wheel
 
 
@@ -45,13 +55,13 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
                                               100, // small error range timeout, in milliseconds
                                               3, // large error range, in inches
                                               500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
+                                              .5 // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              10, // derivative gain (kD)
+lemlib::ControllerSettings angular_controller(2.45, // proportional gain (kP)
+                                              0.0, // integral gain (kI)
+                                              10.5, // derivative gain (kD)
                                               3, // anti windup
                                               1, // small error range, in degrees
                                               100, // small error range timeout, in milliseconds
@@ -63,13 +73,13 @@ lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
 // input curve for throttle input during driver control
 lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
                                      10, // minimum output where drivetrain will move out of 127
-                                     1.019 // expo curve gain
+                                     1.05// expo curve gain
 );
 
 // input curve for steer input during driver control
 lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
                                   10, // minimum output where drivetrain will move out of 127
-                                  1.019 // expo curve gain
+                                  1.05 // expo curve gain
 );
 
 // create the chassis
